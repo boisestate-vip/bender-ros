edb240eeef979fc0b6d1267b7a3c9dbdc5b40a87
camronCollinsworth <camroncollinsworth@gmail.com>
2021-09-27 19:24:43 -0700

    fixed bugs and added imports

--- bender_navsensors/src/test.py
+++ bender_navsensors/src/test.py
@@ -1,18 +1,24 @@
 import rospy
-   4 from std_msgs.msg import String
-   5 
-   6 def talker():
-   7     pub = rospy.Publisher('chatter', String, queue_size=10)
-   8     rospy.init_node('talker', anonymous=True)
-   9     rate = rospy.Rate(10) # 10hz
-  10     while not rospy.is_shutdown():
-  11         hello_str = "hello world %s" % rospy.get_time()
-  12         rospy.loginfo(hello_str)
-  13         pub.publish(hello_str)
-  14         rate.sleep()
-  15 
-  16 if __name__ == '__main__':
-  17     try:
-  18         talker()
-  19     except rospy.ROSInterruptException:
-  20         pass
+from numpy import *
+from std_msgs.msg import String
+from mavros_msgs.msg import *
+from mavros_msgs.srv import *
+    
+def bender_navsensors():
+    pub = rospy.Publisher('bender_navsensors', double, queue_size=10)
+    rospy.init_node('bender_navsensors', anonymous=False)
+    rate = rospy.Rate(60) # 60hz
+
+    while not rospy.is_shutdown():
+        hello_str = "hello world %s" % rospy.get_time()
+        rospy.loginfo(hello_str)
+        pub.publish(hello_str)
+    ##    float32 test_altitude = Altitude()
+        pub.publish("dank memes")
+        rate.sleep()
+   
+if __name__ == '__main__':
+       try:
+          bender_navsensors()
+       except rospy.ROSInterruptException:
+           pass
