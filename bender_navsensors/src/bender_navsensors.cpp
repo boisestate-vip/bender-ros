4f5c71943c7f62f9e2b8c038311f3cb32db3b2d4
camronCollinsworth <77373781+camronCollinsworth@users.noreply.github.com>
2022-05-16 10:41:17 -0600

    Update bender_navsensors.cpp

--- bender_navsensors/src/bender_navsensors.cpp
+++ bender_navsensors/src/bender_navsensors.cpp
@@ -1,52 +1,85 @@
 #include <ros/ros.h>
 #include <mavros_msgs/CommandBool.h>
 #include <mavros_msgs/State.h>
+#include <sensor_msgs/NavSatFix.h>
 #include <std_msgs/Bool.h>
+#include <std_msgs/Float64.h>
 
 mavros_msgs::State state;
+double lat, long;
+double init_lat, init_long;
+double x_dist, y_dist;
+bool init;
+
 void get_state(const mavros_msgs::State::ConstPtr& msg)
 {
-	state = *msg;
+    state = *msg;
 }
+
+void get_gps(const sensor_msgs::NavSatFix::ConstPtr& msg)
+{
+      lat = *msg.latitude.data;
+      long = *msg.longitude.data;
+
+      if (!init)
+      {
+          init_lat = lat;
+          init_long = long;
+          init = true;
+          return;
+      }
+
+      // Math here to turn current lat/long and initial value into meters, save into x_dist and y_dist
+}      
 
 int main (int argc, char **argv)
 {
 
-	ros::init(argc, argv, "navsensors");
-	ros::NodeHandle nh;
+        init = false;
 
-	ros::Subscriber state_listener = nh.subscribe<mavros_msgs::State>("mavros/state", 10, get_state);
+    ros::init(argc, argv, "navsensors");
+    ros::NodeHandle nh;
 
-	ros::ServiceClient arming = nh.serviceClient<mavros_msgs::CommandBool>("mavros/cmd/arming");
+    ros::Subscriber state_listener = nh.subscribe<mavros_msgs::State>("mavros/state", 10, get_state);
+        ros::Subscriber gps_listener = nh.subscribe<sensor_msgs::NavSatFix>("mavros/global_position/global", 10, get_gps);
 
-	ros::Publisher armed_pub = nh.advertise<std_msgs::Bool>("bender_navsensors/state/enabled", 10);
-	ros::Publisher conn_pub = nh.advertise<std_msgs::Bool>("bender_navsensors/state/connected", 10);
+    ros::ServiceClient arming = nh.serviceClient<mavros_msgs::CommandBool>("mavros/cmd/arming");
+
+    ros::Publisher armed_pub = nh.advertise<std_msgs::Bool>("bender_navsensors/state/enabled", 10);
+    ros::Publisher conn_pub = nh.advertise<std_msgs::Bool>("bender_navsensors/state/connected", 10);
+        ros::Publisher x_pub = nh.advertise<std_msgs::Float64>("bender_navsensors/position/x", 10);
+        ros::Publisher y_pub = nh.advertise<std_msgs::Float64>("bender_navsensors/position/y", 10);
 
         ros::Rate loop_rate(20);
 
-	mavros_msgs::CommandBool arm;
-	arm.request.value = true;
+    mavros_msgs::CommandBool arm;
+    arm.request.value = true;
 
-	std_msgs::Bool armed;
-	std_msgs::Bool conn;
-
+    std_msgs::Bool armed;
+    std_msgs::Bool conn;
+        std_msgs::Float64 x_msg;
+        std_msgs::Float64 y_msg;
+	
 	while (ros::ok())
-	{
-		armed.data = state.armed;
-		conn.data = state.connected;
-
-		armed_pub.publish(armed);
-		conn_pub.publish(conn);
-
-		if (!armed.data)
-		{
-			arming.call(arm);
-		}
-
-		ros::spinOnce();
-		loop_rate.sleep();
-	}
-
-	return 0;
-}
-
+      {
+          armed.data = state.armed;
+       conn.data = state.connected;
+                  x_msg.data = x_dist;
+                  y_msg.data = y_dist;
+ 
+          armed_pub.publish(armed);
+          conn_pub.publish(conn);
+                  x_pub.publish(x_msg);
+                  y_pub.publish(y_msg);
+          if (!armed.data)
+          {
+              arming.call(arm);
+          }
+ 
+          ros::spinOnce();
+          loop_rate.sleep();
+    }
+ 
+      return 0;
+ 
+ }
