4af71c5af6853997ea8a99f9fa6c5442fb91948e
Boise State VIP Robotics <bender@viprobotics.boisestate.edu>
2022-05-31 14:14:25 -0600

    correct udev rules

--- bender_localization/scripts/px4_udev.sh
+++ bender_localization/scripts/px4_udev.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-echo 'SUBSYSTEM=="tty", ATTRS{idVendor}=="26ac", ATTRS{idProduct}=="0011", SYMLINK+="ttyPixhawk", RUN+="/bin/stty -F /dev/ttyPixhawk 115200 raw -echo"' | sudo tee 10-pixhawk-px4.rules
+echo 'SUBSYSTEM=="tty", ATTRS{idVendor}=="1209", ATTRS{idProduct}=="5740", SYMLINK+="ttyPixhawk", RUN+="/bin/stty -F /dev/ttyPixhawk 115200 raw -echo"' | sudo tee 10-pixhawk-px4.rules
 sudo udevadm control --reload-rules && sudo udevadm trigger
--- bender_perception/scripts/logitech_webcam_udev.sh
+++ bender_perception/scripts/logitech_webcam_udev.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-echo 'KERNEL=="video*", SUBSYSTEM=="video4linux", ATTRS{idVendor}=="046d", ATTRS{idProduct}=="0843", SYMLINK+="videoLogitech"' | sudo tee 10-logitech-webcam.rules
+echo 'KERNEL=="video*", SUBSYSTEM=="video4linux", ATTRS{index}=="0", ATTR{name}=="Logitech Webcam C930e", SYMLINK+="videoLogitech"' | sudo tee 10-logitech-webcam.rules
 sudo udevadm control --reload-rules && sudo udevadm trigger
