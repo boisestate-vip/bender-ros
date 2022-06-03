0463f1879099fbbb78a2e909b03cd618198ea428
Boise State VIP Robotics <viprobotics@boisestate.edu>
2022-06-03 13:13:53 -0600

    test in parkinglot

--- bender_mission/bin/main.py
+++ bender_mission/bin/main.py
@@ -1,8 +1,14 @@
 #!/usr/bin/env python
 
+import rospy
 from bender_mission.commander import Commander
 
 if __name__ == '__main__':
+    rospy.init_node('bender_mission')
     cmd = Commander()
-    cmd.say()
+    cmd.action_client.wait_for_server()
+    rate = rospy.Rate(10);
+    while not rospy.is_shutdown():
+        cmd.execute()
+        rate.sleep()
     
--- bender_mission/src/bender_mission/commander.py
+++ bender_mission/src/bender_mission/commander.py
@@ -1,15 +1,46 @@
 import numpy as np
+from math import pi, cos, sin
+
 import rospy
 import actionlib
 import tf2_ros
+
+from geometry_msgs.msg import PoseStamped, Pose, Point, Quaternion
 from move_base_msgs.msg import MoveBaseAction
-from geometry_msgs.msg import PoseStamped 
+from nav_msgs.msg import Odometry
+from sensor_msgs.msg import NavSatFix
+from std_msgs.msg import Header
 
 class Commander:
     def __init__(self):
         self.data = "hello"
         self.action_client = actionlib.SimpleActionClient('move_base', MoveBaseAction)
-        self.current_goal = MoveBaseAction(PoseStamped())
+        self.current_position = Pose()
+        self.current_gps = NavSatFix()
+        self.start = rospy.get_param("~start")
+        self.goals = rospy.get_param("~goals")
+        self.current_goal = Pose()
+        self.current_goal_action = MoveBaseAction()
+        self.tf_buffer = tf2_ros.Buffer()
+        self.tf_listener = tf2_ros.TransformListener(self.tf_buffer)
+        self.current_position_subscriber = rospy.Subscriber("odom", Odometry, self.odom_callback)
+        self.current_gps_subscriber = rospy.Subscriber("gps/fix", NavSatFix, self.gps_callback)
 
-    def say(self):
-        print(self.data)
+    def odom_callback(self, msg):
+        self.current_position = msg.pose.pose
+        
+    def gps_callback(self, msg):
+        self.current_gps = msg
+        
+    def execute(self): 
+        print(self.start['x'])
+        print(self.start['y'])
+        # for goal in self.goals:
+        #     current_goal_action.target_pose.pose.position.x = goal[0]
+        #     current_goal_action.target_pose.pose.position.y = goal[1]
+        #     action_client.send_goal(current_goal_action)
+
+def latlong_to_meters(lat_current, lat_delta, long_delta)
+    n = lat_delta * 111321.5
+    e = long_delta * cos(lat_current) * 111321.5
+    return n, e
