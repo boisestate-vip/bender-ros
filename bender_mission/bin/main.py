318c3ddc18f3cccfd11c10a9acfba985cc96e376
Boise State VIP Robotics <viprobotics@boisestate.edu>
2022-06-03 19:37:05 -0600

    before motor broke

--- bender_mission/bin/main.py
+++ bender_mission/bin/main.py
@@ -7,7 +7,7 @@ if __name__ == '__main__':
     rospy.init_node('bender_mission')
     cmd = Commander()
     cmd.action_client.wait_for_server()
-    rate = rospy.Rate(10);
+    rate = rospy.Rate(1);
     while not rospy.is_shutdown():
         cmd.execute()
         rate.sleep()
--- bender_mission/src/bender_mission/commander.py
+++ bender_mission/src/bender_mission/commander.py
@@ -1,46 +1,72 @@
+from cmath import isnan
 import numpy as np
 from math import pi, cos, sin
 
 import rospy
 import actionlib
 import tf2_ros
+from tf.transformations import quaternion_from_euler
 
 from geometry_msgs.msg import PoseStamped, Pose, Point, Quaternion
-from move_base_msgs.msg import MoveBaseAction
+from move_base_msgs.msg import MoveBaseAction, MoveBaseGoal
 from nav_msgs.msg import Odometry
 from sensor_msgs.msg import NavSatFix
-from std_msgs.msg import Header
+from std_msgs.msg import Header, Float64
 
 class Commander:
     def __init__(self):
         self.data = "hello"
         self.action_client = actionlib.SimpleActionClient('move_base', MoveBaseAction)
-        self.current_position = Pose()
         self.current_gps = NavSatFix()
+        self.home_heading = np.NaN
+        self.home_gps = NavSatFix()
         self.start = rospy.get_param("~start")
         self.goals = rospy.get_param("~goals")
-        self.current_goal = Pose()
-        self.current_goal_action = MoveBaseAction()
+        self.current_goal = PoseStamped()
         self.tf_buffer = tf2_ros.Buffer()
         self.tf_listener = tf2_ros.TransformListener(self.tf_buffer)
-        self.current_position_subscriber = rospy.Subscriber("odom", Odometry, self.odom_callback)
+        self.compass_subscriber = rospy.Subscriber("compass/heading", Float64, self.compass_callback)
         self.current_gps_subscriber = rospy.Subscriber("gps/fix", NavSatFix, self.gps_callback)
-
-    def odom_callback(self, msg):
-        self.current_position = msg.pose.pose
         
     def gps_callback(self, msg):
         self.current_gps = msg
-        
-    def execute(self): 
-        print(self.start['x'])
-        print(self.start['y'])
-        # for goal in self.goals:
-        #     current_goal_action.target_pose.pose.position.x = goal[0]
-        #     current_goal_action.target_pose.pose.position.y = goal[1]
-        #     action_client.send_goal(current_goal_action)
 
-def latlong_to_meters(lat_current, lat_delta, long_delta)
-    n = lat_delta * 111321.5
-    e = long_delta * cos(lat_current) * 111321.5
-    return n, e
+    def compass_callback(self, msg):
+        if isnan(self.home_heading):
+            self.home_heading = msg.data*pi/180.0
+
+    def action_callback(self, fbk):
+        return None
+        
+    def execute(self):
+        if isnan(self.home_heading):
+            rospy.logwarn_throttle(1.0, "Waiting to receive initial heading..")
+            return
+        for goal in self.goals:
+            delta_lat  = goal[0] - self.start['lat'] # self.home_gps.latitude
+            delta_long = goal[1] - self.start['long'] # self.home_gps.longitude
+            desired_point = self.latlong_to_meters(self.current_gps.latitude, delta_lat, delta_long)
+            desired_point[1] = -desired_point[1] 
+            self.current_goal.pose.position.x = desired_point[0]
+            self.current_goal.pose.position.y = desired_point[1]
+            self.current_goal.pose.orientation = Quaternion(*quaternion_from_euler(0,0,self.home_heading))
+            self.current_goal.header = Header()
+            self.current_goal.header.stamp = rospy.Time.now()
+            self.current_goal.header.frame_id = "map"
+            print(desired_point)
+            
+            move_base_goal = MoveBaseGoal()
+            move_base_goal.target_pose = self.current_goal
+            self.action_client.send_goal(goal=move_base_goal, feedback_cb=self.action_callback)
+            self.action_client.wait_for_result()
+
+    def latlong_to_meters(self, lat_current, lat_delta, long_delta):
+        n = lat_delta * 111321.5
+        e = long_delta * cos(lat_current*pi/180.0) * 111321.5
+        print([n,e])
+        R = np.array([
+            [ cos(-self.home_heading), -sin(-self.home_heading)],
+            [ sin(-self.home_heading),  cos(-self.home_heading)]
+        ])
+        return np.dot(R, np.array([n,e]))
+        # hdg = self.home_heading
